b5412459c38d2ed61751321c9914ee8305c52648
MoonSangDon <moon3225@naver.com>
2022-02-14 19:12:34 +0900

    newAPI

--- Practice1/Astar.cpp
+++ Practice1/Astar.cpp
@@ -1,24 +1,24 @@
-#include "Astar.h"
-#include <iostream>
-#include <list>
-using namespace std;
-
-int main()
-{
-	list<int> NQ;
-	list<int>::iterator iter;
-
-	NQ.push_back(11);
-	NQ.push_back(22);
-	NQ.push_back(33);
-
-	
-
-
-	for (iter = NQ.begin(); iter != NQ.end(); iter++)
-	{
-		cout << *iter << endl;
-	}
-	
-	return 0;
-}//삼항연산자 >> if else 의 축소판 약간 카빈버전
+//#include "Astar.h"
+//#include <iostream>
+//#include <list>
+//using namespace std;
+//
+//int main()
+//{
+//	list<int> NQ;
+//	list<int>::iterator iter;
+//
+//	NQ.push_back(11);
+//	NQ.push_back(22);
+//	NQ.push_back(33);
+//
+//	
+//
+//
+//	for (iter = NQ.begin(); iter != NQ.end(); iter++)
+//	{
+//		cout << *iter << endl;
+//	}
+//	
+//	return 0;
+//}//삼항연산자 >> if else 의 축소판 약간 카빈버전
